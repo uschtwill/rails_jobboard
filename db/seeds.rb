c58f1581cc191de37468bb12d9a7d2bf3ce7ec3c
Wilhelm Uschtrin <uschtrinw@gmail.com>
2015-02-23 18:39:54 +0300

    adds some entries to the seed-file for seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Job.create(title: "Burger Flipper #2", company: "Apple Inc.", url: "http://www.apple1.com", description:"Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows.")
+
+Job.create(title: "Burger Flipper #2", company: "Apple Inc.", url: "http://www.apple2.com", description:"Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows.")
+
+Job.create(title: "Burger Flipper #2", company: "Apple Inc.", url: "http://www.apple3.com", description:"Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows.")
+
+Job.create(title: "Burger Flipper #2", company: "Apple Inc.", url: "http://www.apple4.com", description:"Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows.")
+
+Job.create(title: "Burger Flipper #2", company: "Apple Inc.", url: "http://www.apple5.com", description:"Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows.")
