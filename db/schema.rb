f4108fdd2a33a4f364fb758634fa2596caabf360
Wilhelm Uschtrin <uschtrinw@gmail.com>
2015-02-23 18:12:43 +0300

    gets rid of FAB button and is a save before experimenting with migrations and dropping the database.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150218072102) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "company"
