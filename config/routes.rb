a38891302da05bf4135428dd0f5e3880a1d354cc
Wilhelm Uschtrin <uschtrinw@gmail.com>
2015-02-24 11:59:34 +0300

    adds devise company(user) model

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@
 #
 
 Rails.application.routes.draw do
+  devise_for :companies
   resources :jobs
   root 'jobs#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218072102) do
+ActiveRecord::Schema.define(version: 20150224085830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "username"
+    t.string   "website"
+    t.string   "phone"
+    t.string   "twitter"
+    t.string   "facebook"
+    t.string   "location"
+    t.string   "industry"
+    t.text     "vision"
+    t.text     "mission"
+    t.integer  "founded"
+    t.integer  "employees"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "companies", ["email"], name: "index_companies_on_email", unique: true, using: :btree
+  add_index "companies", ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true, using: :btree
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -35,15 +64,6 @@ ActiveRecord::Schema.define(version: 20150218072102) do
     t.text     "application"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
   end
 
 end
